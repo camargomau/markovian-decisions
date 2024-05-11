b0c098852fb21a9544ae132ee6361b635d1cd49e
Mau Camargo <52770775+camargomau@users.noreply.github.com>
2024-05-11 16:36:43 -0600

    Refactoring of code

--- input_types.py
+++ input_types.py
@@ -3,23 +3,76 @@ Provides functions that handle input with various data types.
 """
 
 
-def integer(prompt):
+def integer(prompt, min=None, max=None, size=1, separator=","):
     """
-    Function for handling the input of an integer.
+    Function for handling the input of an integer (or an array thereof), with optional min and max constraints.
+    """
+
+    if size == 1:
+        while True:
+            try:
+                answer = int(input(prompt))
+                if min is not None and answer < min:
+                    raise ValueError
+                elif max is not None and answer > max:
+                    raise ValueError
+                break
+
+            except ValueError:
+                if min is not None and max is not None:
+                    prompt = f"-> Introduzca un número entero en [{min}, {max}]: "
+                elif min is not None:
+                    prompt = f"-> Introduzca un número entero mayor o igual que{min}: "
+                elif max is not None:
+                    prompt = f"-> Introduzca un número entero menor o igual que {min}: "
+                else:
+                    prompt = "-> Introduzca un número entero: "
+        return answer
+    else:
+        while True:
+            answer = input(prompt).strip().split(separator)
+            if size != 0 and len(answer) != size:
+                prompt = f"-> Introduzca exactamente {size} enteros: "
+                continue
+
+            try:
+                answer_list = []
+                for num in answer:
+                    num = int(num)
+                    if min is not None and num < min:
+                        raise ValueError
+                    elif max is not None and num > max:
+                        raise ValueError
+                    answer_list.append(num)
+
+                break
+
+            except ValueError:
+                if min is not None and max is not None:
+                    prompt = f"-> Introduzca números enteros en [{min}, {max}]: "
+                elif min is not None:
+                    prompt = f"-> Introduzca números enteros mayores o iguales que{min}: "
+                elif max is not None:
+                    prompt = f"-> Introduzca número enteros menores o iguales que {min}: "
+                else:
+                    prompt = "-> Introduzca números enteros: "
+
+        return answer_list
+
+
+
+def floating(prompt):
+    """
+    Function for handling the input of a float.
     """
 
     while True:
         try:
-            answer = int(input(prompt))
-
-            if answer < 1:
-                raise IndexError
-
+            answer = float(input(prompt))
             break
-        except IndexError:
-            prompt = "-> Introduzca un número entero mayor o igual que 1: "
+
         except ValueError:
-            prompt = "-> Introduzca un número entero: "
+            prompt = "-> Introduzca un número real: "
 
     return answer
 
--- process.py
+++ process.py
@@ -18,33 +18,48 @@ class Process:
 		self.policies = []
 
 
-
 def create_process():
 	# Estados y decisiones
-	states_amount = input_types.integer("• Introduzca la cantidad de estados que tiene el proceso (empiezan en 0): ")
-	decisions_amount = input_types.integer("• Introduzca la cantidad de decisiones que se pueden tomar (empiezan en 1): ")
+	states_amount = input_types.integer("• Introduzca la cantidad de estados que tiene el proceso (empiezan en 0): ", 0)
+	decisions_amount = input_types.integer("• Introduzca la cantidad de decisiones que se pueden tomar (empiezan en 1): ", 0)
 	states = [state for state in range(states_amount)]
 	decisions = [decision+1 for decision in range(decisions_amount)]
 
 	# Aplicabilidad de decisiones
-	all_decisions = input_types.boolean("\n• ¿Todas las decisiones son aplicables para todos los estados? [S]í, [N]o: ", "S", "N")
-	if all_decisions:
-		decision_applicability = [None] + [[True for _ in states] for _ in decisions]
-	else:
-		decision_applicability = [None] + [define_decision_applicability(decision, states) for decision in decisions]
+	decision_applicability = define_decision_applicability(decisions, states)
 
 	# Costos
-	costs = [[None] + [define_costs(decision, state) if decision_applicability[decision][state] else None for decision in decisions] for state in states]
+	costs = define_costs(decisions, states, decision_applicability)
 
 	return Process(states, decisions, decision_applicability, costs)
 
-def define_decision_applicability(decision, states):
-	state_list = input(f"--> Introduzca la lista de estados aplicables para la decisión {decision} (separe con comas): ").split(",")
-	state_list = [int(state) for state in state_list]
 
-	return [True if state in state_list else False for state in states]
+def define_decision_applicability(decisions, states):
+	all_decisions = input_types.boolean("\n• ¿Todas las decisiones son aplicables para todos los estados? [S]í, [N]o: ", "S", "N")
 
-def define_costs(decision, state):
-	return input(f"• Introduzca el costo de tomar la decisión {decision} en el estado {state}: ")
+	# Si todas las decisiones son aplicables, todo es True
+	if all_decisions:
+		return [None] + [[True for _ in states] for _ in decisions]
+	# Si no, poner True donde son aplicables y False donde no
+	else:
+		decision_applicability = [None]
+		for decision in decisions:
+			state_list = input_types.integer(f"--> Introduzca la lista de estados aplicables para la decisión {decision} (separe con comas): ", min=0, size=0)
+			decision_applicability.append([True if state in state_list else False for state in states])
+		return decision_applicability
+
+
+def define_costs(decisions, states, decision_applicability):
+	costs = [[None] + [None for _ in decisions] for _ in states]
+
+	# Donde no hay un costo, hay None. Nótese que un costo de 0 != None
+	for state in states:
+		print(f"• Costos estando en el estado {state}")
+		for decision in decisions:
+			if decision_applicability[decision][state]:
+				costs[state][decision] = input_types.floating(f"-> Introduzca el costo de la decisión {decision}: ")
+
+	return costs
+
 
 process = create_process()
