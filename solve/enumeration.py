7139ce94311f617d0107cbdb0b096e8db2e19d08
Mau Camargo <52770775+camargomau@users.noreply.github.com>
2024-05-21 19:00:49 -0600

    Add matrix splicing for exhaustive enumeration

--- solve/enumeration.py
+++ solve/enumeration.py
@@ -1,4 +1,10 @@
+def solve_steady_state(process, policy):
+	spliced_matrix = [process.transition[decision][origin_state] for origin_state, decision in enumerate(policy)]
+	print(spliced_matrix)
+
 def main(process):
 	process.generate_policies()
 	print(process.policies)
+
+	solve_steady_state(process, process.policies[1])
 	input()
