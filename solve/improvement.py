5a4bc4e8c040c365d5cf594295c6eb9e4adba3ff
Mau Camargo <52770775+camargomau@users.noreply.github.com>
2024-05-22 22:49:00 -0600

    Format improvement.py with autopep8

--- solve/improvement.py
+++ solve/improvement.py
@@ -16,9 +16,11 @@ def solve_system(process, current_policy):
         # los primeros m+1 elementos corresponden a los coeficientes de las V_j para todo estado (j = target_state)
         for target_state in process.states:
             if target_state == state:
-                equation.append(discount*process.transition[decision_in_state][state][target_state]-1)
+                equation.append(
+                    discount*process.transition[decision_in_state][state][target_state]-1)
             else:
-                equation.append(discount*process.transition[decision_in_state][state][target_state])
+                equation.append(
+                    discount*process.transition[decision_in_state][state][target_state])
 
         # En el método estándar, g (costo a largo plazo) está en el sistema
         if discount == 1:
@@ -56,11 +58,14 @@ def find_alt_policy(process, system_solution):
                     # Sin descuento se suma -V_i
                     if discount == 1:
                         if target_state == state:
-                            cost += (process.transition[decision][state][target_state]-1)*system_solution[target_state]
+                            cost += (process.transition[decision][state]
+                                     [target_state]-1)*system_solution[target_state]
                         else:
-                            cost += (process.transition[decision][state][target_state])*system_solution[target_state]
+                            cost += (process.transition[decision][state]
+                                     [target_state])*system_solution[target_state]
                     else:
-                        cost += (discount*process.transition[decision][state][target_state])*system_solution[target_state]
+                        cost += (discount*process.transition[decision][state]
+                                 [target_state])*system_solution[target_state]
 
                 costs_to_compare[decision] = cost
 
@@ -77,7 +82,8 @@ def improve(iter, process, current_policy):
     system_solution = solve_system(process, current_policy)
 
     # Agregar el renglón para la iteración actual
-    row = [iter] + [round(value, 6) for value in system_solution] + [current_policy]
+    row = [iter] + [round(value, 6)
+                    for value in system_solution] + [current_policy]
     table.add_row(row)
 
     # Encontrar una política alternativa a partir de los resultados
@@ -90,7 +96,8 @@ def improve(iter, process, current_policy):
     # Si son iguales, entonces terminar
     # Cuando es sin descuento, se imprime también g (costo a largo plazo)
     if discount == 1:
-            row = [iter+1] + ["N/A" for _ in process.states] + [round(system_solution[-1], 6), current_policy]
+        row = [iter+1] + ["N/A" for _ in process.states] + \
+            [round(system_solution[-1], 6), current_policy]
     else:
         if alt_policy == current_policy:
             row = [iter+1] + ["N/A" for _ in process.states] + [current_policy]
@@ -103,20 +110,23 @@ def main(process, discounted):
     print("Mejoramiento de políticas con descuento\n" if discounted else "Mejoramiento de políticas\n")
 
     # Solicitar la política inicial
-    initial_policy = inp.number("• Introduzca la política inicial (separe con comas): ", min_value=1, max_value=process.decisions[-1], size=len(process.states))
+    initial_policy = inp.number("• Introduzca la política inicial (separe con comas): ",
+                                min_value=1, max_value=process.decisions[-1], size=len(process.states))
 
     # discount es 1 si se eligió el método estándar
     global discount
     discount = 1
     if discounted:
-        discount = inp.number("• Introduzca el descuento (del 0 al 1): ", number_type="f", min_value=0, max_value=1)
+        discount = inp.number("• Introduzca el descuento (del 0 al 1): ",
+                              number_type="f", min_value=0, max_value=1)
         if discount == 1:
             print("\n(descuento de 1: utilizando mejoramiento sin descuento)")
 
     # Tabla donde ir agregando los resultados de cada iteración
     global table
     table = PrettyTable()
-    field_names = ["iter."] + [f"V{i}" for i in process.states] + (["g (costo)"] if discount == 1 else []) + ["Política"]
+    field_names = ["iter."] + [f"V{i}" for i in process.states] + \
+        (["g (costo)"] if discount == 1 else []) + ["Política"]
     table.field_names = field_names
 
     optimal_policy = improve(1, process, initial_policy)
